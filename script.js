a4afa1d8162dd05301b0d66baa517d3ca2b32ca7
Deyvyd Sousa <deyvydvsousa@gmail.com>
2025-11-16 13:05:44 -0300

    Adiciona o script do exercicio

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-window.prompt('teste')
+let lista = [];
+let entrada = "";
+
+while (entrada !== "sair") {
+  entrada = prompt("Digite sua tarefa (Digite 'sair' para encerrar)");
+  if (entrada !== "sair") {
+    lista.push(entrada);
+  }
+}
+alert("Tarefas:\n" + lista.join("\n"));
+
